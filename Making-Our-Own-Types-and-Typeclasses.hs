e61959b8ed2fcb681f5f22723648aeac9e7ec9cc
leichunfeng <307213080@qq.com>
2015-08-26 11:53:34 +0800

    Perfect Making-Our-Own-Types-and-Typeclasses.hs

--- Making-Our-Own-Types-and-Typeclasses.hs
+++ Making-Our-Own-Types-and-Typeclasses.hs
@@ -1,3 +1,6 @@
+import qualified Data.Map as Map
+import Data.Char
+
 -----------------------------
 -- Algebraic data types intro
 -----------------------------
@@ -28,7 +31,7 @@ baseRect width height = Rectangle (Point 0 0) (Point width height)
 ----------------
 
 --data Person = Person String String Int Float String String deriving (Show)
-data Person = Person { firstName :: String, lastName :: String, age :: Int, height :: Float, phoneNumber :: String, flavor :: String } deriving (Show)
+--data Person = Person { firstName :: String, lastName :: String, age :: Int, height :: Float, phoneNumber :: String, flavor :: String } deriving (Show)
 
 --firstName :: Person -> String
 --firstName (Person firstname _ _ _ _ _) = firstname
@@ -79,76 +82,186 @@ scalarMult :: (Num t) => Vector t -> Vector t -> t
 -- Derived instances
 --------------------
 
+data Person = Person { firstName :: String, lastName :: String, age :: Int } deriving (Eq, Show, Read)
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
 ----------------
 -- Type synonyms
 ----------------
+
+type Name = String
+type PhoneNumber = String
+type PhoneBook = [(Name,PhoneNumber)]
+
+phoneBook :: PhoneBook
+phoneBook = [("betty","555-2938"),("bonnie","452-2928"),("patsy","493-2928"),("lucille","205-2928"),("wendy","939-8282"),("penny","853-2492")]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook
+
+type AssocList k v = [(k,v)]
+
+type IntMap = Map.Map Int
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
+        Just (state, code) -> if state /= Taken
+                                then Right code
+                                else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList [(100,(Taken,"ZD39I")),(101,(Free,"JAH3I")),(103,(Free,"IQSA9")),(105,(Free,"QOTSA")),(109,(Taken,"893JJ")),(110,(Taken,"99292"))]
 
 ----------------------------
 -- Recursive data structures
 ----------------------------
 
+--data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+--data List a = Empty | Cons { listHead :: a, listTail :: List a } deriving (Show, Read, Eq, Ord)
+
+--infixr 5 ++
+--(++) :: [a] -> [a] -> [a]
+--[]     ++ ys = ys
+--(x:xs) ++ ys = x : (xs ++ ys)
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty      .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
 ------------------
 -- Typeclasses 102
 ------------------
+
+--class Eq a where
+--    (==) :: a -> a -> Bool
+--    (/=) :: a -> a -> Bool
+--    x == y = not (x /= y)
+--    x /= y = not (x == y)
+
+data TrafficLight = Red | Green | Yellow
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Green = "Green light"
+    show Yellow = "Yellow light"
+
+--instance Eq (Maybe m) where
+--    Just x == Just y = x == y
+--    Nothing == Nothing = True
+--    _ == _ = False
+
+--instance (Eq m) => Eq (Maybe m) where
+--    Just x == Just y = x == y
+--    Nothing == Nothing = True
+--    _ == _ = False
 
 ---------------------
 -- A yes-no typeclass
 ---------------------
 
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
+
 ------------------------
 -- The Functor typeclass
 ------------------------
+
+--class Functor f where
+--    fmap :: (a -> b) -> f a -> f b
+
+--instance Functor [] where
+--    fmap = map
+
+--instance Functor Maybe where
+--    fmap f (Just x) = Just (f x)
+--    fmap f Nothing = Nothing
+
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x leftsub rightsub) = Node (f x) (fmap f leftsub) (fmap f rightsub)
+
+--instance Functor (Either a) where
+--    fmap f (Right x) = Right (f x)
+--    fmap f (Left x) = Left x
 
 --------------------------
 -- Kinds and some type-foo
 --------------------------
 
+class Tofu t where
+    tofu :: j a -> t a j
 
+data Frank a b = Frank { frankField :: b a } deriving (Show)
 
+instance Tofu Frank where
+    tofu x = Frank x
 
+data Barry t k p = Barry { yabba :: p, dabba :: t k }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+instance Functor (Barry a b) where
+    fmap f (Barry {yabba = x, dabba = y}) = Barry {yabba = f x, dabba = y}
