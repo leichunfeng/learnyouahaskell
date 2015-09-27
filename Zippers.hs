3612172d0c960b22e4a14f3adc356a3803114787
leichunfeng <307213080@qq.com>
2015-09-27 12:43:29 +0800

    Zippers.hs

--- Zippers.hs
+++ Zippers.hs
@@ -1,3 +1,5 @@
+import Data.List (break)
+
 ----------------
 -- Taking a walk
 ----------------
@@ -62,15 +64,15 @@ data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving (Show)
 
 type Breadcrumbs a = [Crumb a]
 
-goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
+--goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+--goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
 
-goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
+--goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+--goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
 
-goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
-goUp (t, RightCrumb x l:bs) = (Node x l t, bs)
+--goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
+--goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
+--goUp (t, RightCrumb x l:bs) = (Node x l t, bs)
 
 type Zipper a = (Tree a, Breadcrumbs a)
 
@@ -81,18 +83,90 @@ modify f (Empty, bs) = (Empty, bs)
 attach :: Tree a -> Zipper a -> Zipper a
 attach t (_, bs) = (t, bs)
 
-topMost :: Zipper a -> Zipper a
-topMost (t,[]) = (t,[])
-topMost z = topMost (goUp z)
+--topMost :: Zipper a -> Zipper a
+--topMost (t,[]) = (t,[])
+--topMost z = topMost (goUp z)
 
 --------------------
 -- Focusing on lists
 --------------------
 
+type ListZipper a = ([a],[a])
+
+goForward :: ListZipper a -> ListZipper a
+goForward (x:xs, bs) = (xs, x:bs)
+
+goBack :: ListZipper a -> ListZipper a
+goBack (xs, b:bs) = (b:xs, bs)
+
 ----------------------------
 -- A very simple file system
 ----------------------------
 
+type Name = String
+type Data = String
+data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
+
+myDisk :: FSItem
+myDisk =
+    Folder "root"
+        [ File "goat_yelling_like_man.wmv" "baaaaaa"
+        , File "pope_time.avi" "god bless"
+        , Folder "pics"
+            [ File "ape_throwing_up.jpg" "bleargh"
+            , File "watermelon_smash.gif" "smash!!"
+            , File "skull_man(scary).bmp" "Yikes!"
+            ]
+        , File "dijon_poupon.doc" "best mustard"
+        , Folder "programs"
+            [ File "fartwizard.exe" "10gotofart"
+            , File "owl_bandit.dmg" "mov eax, h00t"
+            , File "not_a_virus.exe" "really not a virus"
+            , Folder "source code"
+                [ File "best_hs_prog.hs" "main = print (fix error)"
+                , File "random.hs" "main = print 4"
+                ]
+            ]
+        ]
+
+data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
+
+type FSZipper = (FSItem, [FSCrumb])
+
+fsUp :: FSZipper -> FSZipper
+fsUp (item, FSCrumb name ls rs:bs) = (Folder name (ls ++ [item] ++ rs), bs)
+
+
+fsTo :: Name -> FSZipper -> FSZipper
+fsTo name (Folder folderName items, bs) =
+    let (ls, item:rs) = break (nameIs name) items
+    in  (item, FSCrumb folderName ls rs:bs)
+
+nameIs :: Name -> FSItem -> Bool
+nameIs name (Folder folderName _) = name == folderName
+nameIs name (File fileName _) = name == fileName
+
+fsRename :: Name -> FSZipper -> FSZipper
+fsRename newName (Folder name items, bs) = (Folder newName items, bs)
+fsRename newName (File name dat, bs) = (File newName dat, bs)
+
+fsNewFile :: FSItem -> FSZipper -> FSZipper
+fsNewFile item (Folder folderName items, bs) =
+    (Folder folderName (item:items), bs)
+
 ------------------
 -- Watch your step
 ------------------
+
+goLeft :: Zipper a -> Maybe (Zipper a)
+goLeft (Node x l r, bs) = Just (l, LeftCrumb x r:bs)
+goLeft (Empty, _) = Nothing
+
+goRight :: Zipper a -> Maybe (Zipper a)
+goRight (Node x l r, bs) = Just (r, RightCrumb x l:bs)
+goRight (Empty, _) = Nothing
+
+goUp :: Zipper a -> Maybe (Zipper a)
+goUp (t, LeftCrumb x r:bs) = Just (Node x t r, bs)
+goUp (t, RightCrumb x l:bs) = Just (Node x l t, bs)
+goUp (_, []) = Nothing
